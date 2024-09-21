5a93e8a83e6bd490b1495210c033a6f17c5dc0d1
krabs <2367412680@qq.com>
2024-09-21 13:38:53 +0800

    同步

--- 用于测试2.py
+++ 用于测试2.py
@@ -7,8 +7,9 @@ from models.prompt_ir_model import PromptGenBlock
 
 
 if __name__ == '__main__':
-    a = '3;/home/ljp/a-wxk/MicroExpress/MicroExpressionRecognition-master/datasets/train/others/img-30 (60).jpg'.split(';')[1].split('')
-    print(a)
+    print("xiegaotian")
+    print("xiegaotian")
+    print("xiegaotian")
 
 
 
