d7818c9572be6e3e2fbc56ae58e55d8acf0c55ba
krabsng <2367412680@qq.com>
2024-09-21 20:24:29 +0800

    解决镜像共享内存报错的问题

--- models/krabs_model.py
+++ models/krabs_model.py
@@ -663,15 +663,15 @@ class KrabsModel(BaseModel):
 
     def forward(self):
 
-        # 检查模型的第一个参数在哪个设备上
-        print(torch.cuda.device_count())  # 确保有足够数量的 GPU
-        print(torch.cuda.get_device_name(0))  # 检查每个 GPU 的名称
-
-        print(self.opt.gpu_ids)
-        print(next(self.netKrabs.parameters()).device)
-        print(self.Origin_Img.device)
-        for param in self.netKrabs.parameters():
-            print(param.device)
+        # # 检查模型的第一个参数在哪个设备上
+        # print(torch.cuda.device_count())  # 确保有足够数量的 GPU
+        # print(torch.cuda.get_device_name(0))  # 检查每个 GPU 的名称
+        #
+        # print(self.opt.gpu_ids)
+        # print(next(self.netKrabs.parameters()).device)
+        # print(self.Origin_Img.device)
+        # for param in self.netKrabs.parameters():
+        #     print(param.device)
 
         self.Generate_Img = self.netKrabs(self.Origin_Img)
 
