bb60643dcdae328ffa3962ffc1208c44723f0f23
krabsng <2367412680@qq.com>
2024-11-13 00:33:07 +0800

    GAN损失

--- models/spu_gan_model.py
+++ models/spu_gan_model.py
@@ -844,6 +844,6 @@ class SPUGANModel(BaseModel):
             parser.add_argument('--lambda_A', type=float, default=0, help='')  # SSIM
             parser.add_argument('--lambda_B', type=float, default=0, help='')  # netWork
             parser.add_argument('--lambda_C', type=float, default=0, help='')  # L2
-            parser.add_argument('--lambda_D', type=float, default=1, help='')  # 全变差
-            parser.add_argument('--lambda_E', type=float, default=0, help='')  # GAN
+            parser.add_argument('--lambda_D', type=float, default=0, help='')  # 全变差
+            parser.add_argument('--lambda_E', type=float, default=1, help='')  # GAN
         return parser
