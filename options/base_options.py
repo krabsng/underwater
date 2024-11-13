1c1d67f865f283dbf3551c8612b55a154b98be8f
krabsng <2367412680@qq.com>
2024-11-13 15:22:56 +0800

    超分辨率训练SPU

--- options/base_options.py
+++ options/base_options.py
@@ -52,7 +52,7 @@ class BaseOptions():
         parser.add_argument('--serial_batches', action='store_true',
                             help='如果为 true，则拍摄图像以进行批量处理，否则随机拍摄图像')
         parser.add_argument('--num_threads', default=12, type=int, help='# 用于加载数据的线程数量') # 更改为0
-        parser.add_argument('--batch_size', type=int, default=12, help='输入的批量大小') # 要使用的Batch_size
+        parser.add_argument('--batch_size', type=int, default=8, help='输入的批量大小') # 要使用的Batch_size
         parser.add_argument('--load_size', type=int, default=256, help='将图像缩放到此大小')
         parser.add_argument('--crop_size', type=int, default=256, help='然后裁剪到这个大小')
         parser.add_argument('--max_dataset_size', type=int, default=float("inf"),
