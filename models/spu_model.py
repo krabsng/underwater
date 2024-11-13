e455a8fe29991f581b896e730c84d6b1faf858f3
krabsng <2367412680@qq.com>
2024-11-13 15:33:13 +0800

    超分辨率训练SPU

--- models/spu_model.py
+++ models/spu_model.py
@@ -557,10 +557,10 @@ class SPUNet(nn.Module):
         )
         self.reduce_c1 = nn.Conv2d(int(mid_dim * 2 ** 1), int(mid_dim * 2 ** 0), kernel_size=1, bias=True)
 
-        self.up_sr = nn.Sequential(*([DualPathUpsampling(int(mid_dim * 2 ** 0),2,4)] +
-                                     [SwinTransformerBlock(dim=int(mid_dim // 2), num_heads=2, window_size=2)
+        self.up_sr = nn.Sequential(*([DualPathUpsampling(int(mid_dim * 2 ** 0),8,2)] +
+                                     [SwinTransformerBlock(dim=int(mid_dim // 2), num_heads=8, window_size=2)
                                      for i in range(2)]))
-        self.dw_lr = nn.Sequential(*[DualPathDownsampling(int(mid_dim * 2 ** -1),2,4)])
+        self.dw_lr = nn.Sequential(*[DualPathDownsampling(int(mid_dim * 2 ** -1),8,2)])
         self.lr_p = OverlapPatchEmbed(in_c=mid_dim, out_c=out_dim)
         self.sr_p = OverlapPatchEmbed(in_c=mid_dim // 2, out_c=out_dim)
         if self.Prompt:
@@ -739,7 +739,7 @@ class SPUModel(BaseModel):
                       lambda_B * self.network_loss(self.Generate_Img, self.GT_Img) + \
                       lambda_C * self.L1_loss(self.Generate_Img, self.GT_Img)
                       # lambda_D * self.TotalVariation_loss(self.Generate_Img)
-        self.loss_M = 10 * self.loss_M
+        # self.loss_M = 10 * self.loss_M
         self.loss_M.backward()
 
     def optimize_parameters(self):
@@ -765,8 +765,8 @@ class SPUModel(BaseModel):
         """
         parser.set_defaults(no_dropout=True)  # 默认 CycleGAN 不使用 dropout
         if is_train:
-            parser.add_argument('--lambda_A', type=float, default=0.4, help='')  # SSIM
+            parser.add_argument('--lambda_A', type=float, default=0.3, help='')  # SSIM
             parser.add_argument('--lambda_B', type=float, default=0.2, help='')  # netWork
-            parser.add_argument('--lambda_C', type=float, default=0.4, help='')  # L1
+            parser.add_argument('--lambda_C', type=float, default=0.5, help='')  # L1
             parser.add_argument('--lambda_D', type=float, default=0, help='')  # 全变差
         return parser
