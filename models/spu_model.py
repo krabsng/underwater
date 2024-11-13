1032d7cf4d081ede059987ad0cff9dbb52c6ec29
krabsng <2367412680@qq.com>
2024-11-13 15:57:08 +0800

    超分辨率训练SPU

--- models/spu_model.py
+++ models/spu_model.py
@@ -764,8 +764,8 @@ class SPUModel(BaseModel):
         """
         parser.set_defaults(no_dropout=True)  # 默认 CycleGAN 不使用 dropout
         if is_train:
-            parser.add_argument('--lambda_A', type=float, default=0.4, help='')  # SSIM
+            parser.add_argument('--lambda_A', type=float, default=0.3, help='')  # SSIM
             parser.add_argument('--lambda_B', type=float, default=0.1, help='')  # netWork
-            parser.add_argument('--lambda_C', type=float, default=0.5, help='')  # L1
+            parser.add_argument('--lambda_C', type=float, default=0.6, help='')  # L1
             parser.add_argument('--lambda_D', type=float, default=0, help='')  # 全变差
         return parser
